eca1e9f0f2ad6f5a1d45549781309afc57e3265c
Guilherme Berlintes <guilherme.berlintes@outlook.com>
2026-06-10 19:27:26 -0300

    Begin

---
+++ lib/citadel.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "citadel/version"
+
+module Citadel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/citadel/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Citadel
+  VERSION = "0.1.0"
+end
